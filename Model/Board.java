f5bcda677fef1fc8081628f5e962b97f4ba0f4fc
Soham <141822047+XohamOG@users.noreply.github.com>
2024-06-09 22:11:24 +0530

    error free hopefully

--- Model/Board.java
+++ Model/Board.java
@@ -11,18 +11,10 @@ public class Board {
 
     private void initializeBoard() {
         for (int i = 0; i < 8; i++) {
-            board[1][i] = new Piece(PieceColor.White, PieceType.PAWN, 1, i);
-            board[6][i] = new Piece(PieceColor.Black, PieceType.PAWN, 6, i);
+            board[6][i] = new Piece(PieceColor.White, PieceType.PAWN, 6, i);
+            board[1][i] = new Piece(PieceColor.Black, PieceType.PAWN, 1, i);
         }
 
-        board[7][0] = new Piece(PieceColor.White, PieceType.ROOK, 7, 0);
-        board[7][7] = new Piece(PieceColor.White, PieceType.ROOK, 7, 7);
-        board[7][1] = new Piece(PieceColor.White, PieceType.KNIGHT, 7, 1);
-        board[7][6] = new Piece(PieceColor.White, PieceType.KNIGHT, 7, 6);
-        board[7][2] = new Piece(PieceColor.White, PieceType.BISHOP, 7, 2);
-        board[7][5] = new Piece(PieceColor.White, PieceType.BISHOP, 7, 5);
-        board[7][3] = new Piece(PieceColor.White, PieceType.KING, 7, 3);
-        board[7][4] = new Piece(PieceColor.White, PieceType.QUEEN, 7, 4);
 
         board[7][0] = new Piece(PieceColor.White, PieceType.ROOK, 7, 0);
         board[7][7] = new Piece(PieceColor.White, PieceType.ROOK, 7, 7);
@@ -32,6 +24,17 @@ public class Board {
         board[7][5] = new Piece(PieceColor.White, PieceType.BISHOP, 7, 5);
         board[7][3] = new Piece(PieceColor.White, PieceType.KING, 7, 3);
         board[7][4] = new Piece(PieceColor.White, PieceType.QUEEN, 7, 4);
+
+        board[0][0] = new Piece(PieceColor.Black, PieceType.ROOK, 0, 0);
+        board[0][7] = new Piece(PieceColor.Black, PieceType.ROOK, 0, 7);
+        board[0][1] = new Piece(PieceColor.Black, PieceType.KNIGHT, 0, 1);
+        board[0][6] = new Piece(PieceColor.Black, PieceType.KNIGHT, 0, 6);
+        board[0][2] = new Piece(PieceColor.Black, PieceType.BISHOP, 0, 2);
+        board[0][5] = new Piece(PieceColor.Black, PieceType.BISHOP, 0, 5);
+        board[0][3] = new Piece(PieceColor.Black, PieceType.KING, 0, 3);
+        board[0][4] = new Piece(PieceColor.Black, PieceType.QUEEN, 0, 4);
+
+
     }
 
     public void printkaro() {
